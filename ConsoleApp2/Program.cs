725cb142fcb2a125353bfb6ae56c5e47c8acadd1
KenanNagiyev <788lnmp@code.edu.az>
2022-03-04 06:08:20 +0400

    new proekt

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m = ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int say = 0;
+
+            for (int i = n; i < m; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    say++;
+                }
+
+            }
+            Console.WriteLine("say = " + say);
+            Console.ReadLine();
+        }
+    }
+    }
+
